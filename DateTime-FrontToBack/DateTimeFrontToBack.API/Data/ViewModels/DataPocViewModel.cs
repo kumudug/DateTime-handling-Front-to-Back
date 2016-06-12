ed91cafff12dbaabad88db78aa02151632431bda
Kumudu Gunasekara <kumudugunasekara@hotmail.com>
2016-06-12 10:01:20 +1000

    db generation done

---
+++ DateTime-FrontToBack/DateTimeFrontToBack.API/Data/ViewModels/DataPocViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DateTimeFrontToBack.API.Data.ViewModels
+{
+    public class DataPocViewModel
+    {
+        public int Id { get; set; }
+        // ReSharper disable once InconsistentNaming
+        public DateTime StoredDateTimeUTC { get; set; }
+        public DateTimeOffset StoredDateTimeOffset { get; set; }
+    }
+}
---
+++ DateTime-FrontToBack/DateTimeFrontToBack.API/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace DateTimeFrontToBack.API.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DateTimeFrontToBack.API.Data.Context.PocContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DateTimeFrontToBack.API.Data.Context.PocContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
